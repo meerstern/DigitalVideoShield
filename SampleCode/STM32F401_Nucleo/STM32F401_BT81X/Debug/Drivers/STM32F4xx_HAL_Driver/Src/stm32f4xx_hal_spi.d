2e6dd640e6c71b13ed819566a2188001c22bd151
Crescent <kento621@gmail.com>
2019-02-24 11:42:16 +0900

    Add output files

---
+++ SampleCode/STM32F401_Nucleo/STM32F401_BT81X/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.d
@@ -0,0 +1,80 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Inc/stm32f4xx_hal_conf.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/core_cm4.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Inc/stm32f4xx_hal_conf.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
---
+++ SampleCode/STM32F401_Nucleo/STM32F401_BT81X/Debug/Src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/BT8XX.c \
+../Src/gpio.c \
+../Src/main.c \
+../Src/spi.c \
+../Src/stm32f4xx_hal_msp.c \
+../Src/stm32f4xx_it.c \
+../Src/system_stm32f4xx.c \
+../Src/usart.c 
+
+OBJS += \
+./Src/BT8XX.o \
+./Src/gpio.o \
+./Src/main.o \
+./Src/spi.o \
+./Src/stm32f4xx_hal_msp.o \
+./Src/stm32f4xx_it.o \
+./Src/system_stm32f4xx.o \
+./Src/usart.o 
+
+C_DEPS += \
+./Src/BT8XX.d \
+./Src/gpio.d \
+./Src/main.d \
+./Src/spi.d \
+./Src/stm32f4xx_hal_msp.d \
+./Src/stm32f4xx_it.d \
+./Src/system_stm32f4xx.d \
+./Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F401xE -I"D:/30_ARM_WS/workspace/STM32F401_BT81X/Inc" -I"D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/30_ARM_WS/workspace/STM32F401_BT81X/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
